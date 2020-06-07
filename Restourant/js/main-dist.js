29ee6e036eb271e5845332f25b13457b5eb2f78f
Arcian <66460658+Arcian@users.noreply.github.com>
2020-06-08 00:06:21 +0600

    restourant Commit

--- Restourant/js/main-dist.js
+++ Restourant/js/main-dist.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".slider").slick({arrows:!0,dots:!1,slidesToShow:2,responsive:[{breakpoint:994,settings:{slidesToShow:1}}]}),$(".header__burger").click(function(e){$(".header__burger,.header__nav").toggleClass("active"),$("body").toggleClass("lock")})});
+$(document).ready(function () { $(".slider").slick({ arrows: true, dots: !1, slidesToShow: 2, responsive: [{ breakpoint: 994, settings: { slidesToShow: 1 } }] }), $(".header__burger").click(function (e) { $(".header__burger,.header__nav").toggleClass("active"), $("body").toggleClass("lock") }) });
