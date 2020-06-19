a656294cddb6d95f6652e2a745069f4d98a9eabb
Arcian <66460658+Arcian@users.noreply.github.com>
2020-06-19 21:29:04 +0600

    delete a

--- Restourant/js/main-dist.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function () { $(".slider").slick({ arrows: true, dots: !1, slidesToShow: 2, responsive: [{ breakpoint: 994, settings: { slidesToShow: 1 } }] }), $(".header__burger").click(function (e) { $(".header__burger,.header__nav").toggleClass("active"), $("body").toggleClass("lock") }) });
