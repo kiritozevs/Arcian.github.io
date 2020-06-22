f1b738e688ecb701fd4e9fa507662023807e8f33
Arcian <66460658+Arcian@users.noreply.github.com>
2020-06-22 20:31:35 +0600

    new

--- Nastya/js/script.js
+++ Nastya/js/script.js
@@ -21,17 +21,24 @@ const numSlides = document.querySelector('.bar-block__numtwo');
 const lineBar = document.querySelector('.bar-block__linebar');
 const prev = document.querySelector('.swiper-button-prev');
 const next = document.querySelector('.swiper-button-next');
+const accordionShow = document.querySelectorAll('.slider-b-body__accordionshow');
+const goShow = document.querySelectorAll('.slider-b-body__goshow');
 
-
-
+let goShowNum = 0;
 let openNum = 1;
 let offOnNum = 0;
 let openI = 0;
+let openI2 = 0;
+let openI3 = 0;
 let lineBarWidth = 100 / slides.length;
+let button;
 
-numSlides.textContent = slides.length;
+if (numSlides != null) {
+   numSlides.textContent = slides.length;
 
-lineBar.style.width = `${lineBarWidth}%`;
+   lineBar.style.width = `${lineBarWidth}%`;
+
+}
 
 function open() {
 
@@ -83,14 +90,66 @@ function offOn() {
 
 }
 
+if (numSlides != null) {
+   prev.addEventListener('click', () => {
+      let gggg = lineBar.style.width;
+      lineBar.style.width = `${Number(gggg.slice(0, -1)) - lineBarWidth}%`;
+   });
+   next.addEventListener('click', () => {
+      let gggg = lineBar.style.width;
+      lineBar.style.width = `${Number(gggg.slice(0, -1)) + lineBarWidth}%`;
+   });
+}
 arrow.addEventListener('click', open);
 itemOff.addEventListener('click', offOn);
 itemOn.addEventListener('click', offOn);
-prev.addEventListener('click', () => {
-   let gggg = lineBar.style.width;
-   lineBar.style.width = `${Number(gggg.slice(0, -1)) - lineBarWidth}%`;
-});
-next.addEventListener('click', () => {
-   let gggg = lineBar.style.width;
-   lineBar.style.width = `${Number(gggg.slice(0, -1)) + lineBarWidth}%`;
-});
+// accordionShow.forEach(function () {
+//    accordionShow[openI2].addEventListener('click', () => {
+//       if (goShowNum == 0) {
+//          goShow[openI].style.maxHeight = '200px'
+//          goShowNum = 1;
+//          openI++;
+//       } else {
+//          goShow[openI].style.maxHeight = '0px'
+//          goShowNum = 0;
+//          openI++
+//       }
+//       openI = 0;
+//       openI2++
+//    })
+//    openI2 = 0;
+// });
+
+// function clickHandler() {
+
+//    console.log(this.goShow)
+
+// }
+
+// for (openI2 = 0; openI2 < accordionShow.length; openI2++) {
+//    openI3 = openI2;
+//    accordionShow[openI2].addEventListener('click', clickHandler);
+
+// }
+
+// accordionShow.addEventListener('click', () => {
+//    if (goShowNum == 0) {
+//       goShow.style.maxHeight = '200px'
+//       goShowNum = 1;
+//    } else {
+//       goShow.style.maxHeight = '0px'
+//       goShowNum = 0;
+//    }
+// });
+
+
+   // if (goShowNum == 0) {
+   //    goShow[openI3].style.maxHeight = '200px'
+   //    goShowNum = 1;
+   //    openI++;
+   // } else {
+   //    goShow[openI3].style.maxHeight = '0px'
+   //    goShowNum = 0;
+   //    openI++
+   // }
+   // openI = 0;
