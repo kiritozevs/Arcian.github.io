dd258f76b06b99772a7e9d41caf383a17a2a0b0e
Arcian <66460658+Arcian@users.noreply.github.com>
2020-08-31 19:37:10 +0600

    newcity

--- artur/phps/mail.php
+++ artur/phps/mail.php
@@ -11,14 +11,14 @@ $email = $_POST['user_email'];
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.yandex.ru';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'asfsafsadfdsfsdaf@yandex.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Username = 'sdfgsaggasgfwagawrg@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
 $mail->Password = 'legadagane'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('asfsafsadfdsfsdaf@yandex.ru'); // от кого будет уходить письмо?
+$mail->setFrom('sdfgsaggasgfwagawrg@mail.ru'); // от кого будет уходить письмо?
 $mail->addAddress('schafikov.arthur@yandex.ru');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
